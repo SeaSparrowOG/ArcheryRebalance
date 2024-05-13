7f8b45765fb2b85b962c6fa9cb4ad1d44d752ccb
SeaSparrow <contactMeOnNexus@gmail.com>
2024-05-13 21:32:33 +0300

    Updated to PO3's Commonlib fork

---
+++ cmake/headerlist.cmake
@@ -0,0 +1,7 @@
+set(headers ${headers}
+	include/PCH.h
+	include/equipmentAdjuster.h
+	include/eventHandler.h
+	include/installHook.h
+	include/settings.h
+)
---
+++ cmake/sourcelist.cmake
@@ -0,0 +1,7 @@
+set(sources ${sources}
+	src/equipmentAdjuster.cpp
+	src/eventHandler.cpp
+	src/installHook.cpp
+	src/main.cpp
+	src/settings.cpp
+)
